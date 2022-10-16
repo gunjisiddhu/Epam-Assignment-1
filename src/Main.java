c624825fdd99edc6061c4def37dcba4562d9800b
Siddhu <gunjisiddhu@gmail.com>
2022-10-16 17:20:44 +0530

    Commit first time

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int siddhu;
+        siddhu = 4196;
+
+        System.out.println("This Project is made by siddhu ");
+        System.out.println("My Roll Nummber is "+siddhu);
+    }
+}
